532c6a4d1ce20c7db03ca46a1fafdeca7154d7f9
Tys vanZeyl <ttis3.14@gmail.com>
2019-12-28 22:23:28 -0800

    main now takes longform uci to send to stockfish (in std

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
 """This program acts as a Chess Engine
 
 Sends Universal Chess Interface commands to UniversalChessEngine through SYS.stdin"""
+
+import StockfishInterface
+
+interface = StockfishInterface.StockfishInterface(10)
+while True:
+    interface.move(input("move: "))
--- StockfishInterface.py
+++ StockfishInterface.py
@@ -1,49 +1,55 @@
 import subprocess
 import time
 
-engine = subprocess.Popen(
-    'stockfish_10_x64.exe',
-    universal_newlines=True,
-    bufsize=1,
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-)
 
+class StockfishInterface:
+    def __init__(self, skill_level=20):
+        # define the engine
+        self.engine = subprocess.Popen(
+            'stockfish_10_x64.exe',
+            universal_newlines=True,
+            bufsize=1,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        )
+        # set position string
+        self.position = "position startpos moves"
+        # start the engine
+        self.get()
+        self.put('uci')
+        self.get()
+        self.put('setoption name Hash value 128')
+        self.get()
+        self.put("setoption name Skill Level value " + str(skill_level))  # 0-20
+        self.get()
+        self.put('ucinewgame')
+        self.get()
 
-def put(command):
-    print('\nyou:\n\t'+command)
-    engine.stdin.write(command+'\n')
+    def put(self, command):
+        print('\nyou:\n\t' + command)
+        self.engine.stdin.write(command + '\n')
 
+    def get(self):
+        # using the 'isready' command (engine has to answer 'readyok')
+        # to indicate current last line of stdout
+        self.engine.stdin.write('isready\n')
+        return_string = ""
+        print('\nengine:')
+        while True:
+            text = self.engine.stdout.readline().strip()
+            if text == 'readyok':
+                break
+            if text != '':
+                print('\t' + text)
+                return_string += text
+        return
 
-def get():
-    # using the 'isready' command (engine has to answer 'readyok')
-    # to indicate current last line of stdout
-    engine.stdin.write('isready\n')
-    print('\nengine:')
-    while True:
-        text = engine.stdout.readline().strip()
-        if text == 'readyok':
-            break
-        if text !='':
-            print('\t'+text)
-
-
-skill_level = 20
-
-get()
-put('uci')
-get()
-put('setoption name Hash value 128')
-get()
-put("setoption name Skill Level value " + str(skill_level))  # 0-20
-get()
-put('ucinewgame')
-get()
-put('position startpos moves e2e4 e7e5 f2f4')
-get()
-put('go infinite')
-time.sleep(3)
-get()
-put('stop')
-get()
-put('quit')
+    def move(self, uci_string):
+        self.position += " " + uci_string
+        self.put(self.position)
+        self.get()
+        self.put('go movetime 1000')
+        time.sleep(1.1)
+        self.get()
+        # self.put('stop')
+        # self.get()
