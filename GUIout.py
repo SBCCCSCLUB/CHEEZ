f7d00512ad448fe423ad5c0a082bd5c7fbbaabe3
Tys vanZeyl <ttis3.14@gmail.com>
2020-01-07 22:01:54 -0800

    added 100x100px gifs of chess pieces to /chessPieces

--- GUIout.py
+++ GUIout.py
@@ -5,3 +5,20 @@ basically it shows the ChessBoard Object's current state
 (and therefore what the computer thinks is happening), graphically
 
 TODO write it"""
+
+from Tkinter import Tk, Frame, Canvas
+import ImageTk
+
+t = Tk()
+t.title("Transparency")
+
+frame = Frame(t)
+frame.pack()
+
+canvas = Canvas(frame, bg="black", width=500, height=500)
+canvas.pack()
+
+photoimage = ImageTk.PhotoImage(file="chessPieces/bdd.png")
+canvas.create_image(150, 150, image=photoimage)
+
+t.mainloop()
