7de5c9d48e422d379ca4021419b7e08a051440db
Tys vanZeyl <ttis3.14@gmail.com>
2020-01-07 23:23:02 -0800

    works but the box is not resizable and its sideways

--- GUIout.py
+++ GUIout.py
@@ -13,19 +13,35 @@ import tkinter as tk
 class ChessGUI:
     def __init__(self, chess_board: ChessBoard.ChessBoard):
         self.this_board = chess_board
-        for row in self.this_board.board_array:
-            for square in row:
-                if square is not None:
-                    print(square.FEN)
+        self.root = tk.Tk()
+        self.image_array = [[None for i in range(8)] for j in range(8)]
+        for rank_row_index in range(7, -1, -1):
+            for file_column_index in range(8):
+                image_name = "chessPieces/"
+                # is there a piece on the tile?
+                if self.this_board.board_array[rank_row_index][file_column_index] is not None:
+                    # which piece?
+                    image_name += self.this_board.board_array[rank_row_index][file_column_index].FEN.lower()
+                    # which color piece?
+                    if self.this_board.board_array[rank_row_index][file_column_index].FEN.islower():
+                        image_name += "d"
+                    else:
+                        image_name += "l"
+                # what color is the tile?
+                if (file_column_index + rank_row_index) % 2 != 0:
+                    image_name += "l"
                 else:
-                    print()
+                    image_name += "d"
+                # place the tile (and piece)
+                image_name += ".gif"
+                self.image_array[rank_row_index][file_column_index] = tk.PhotoImage(file=image_name)
+                label = tk.Label(image=self.image_array[rank_row_index][file_column_index])
+                label.place(x=100*rank_row_index, y=100*file_column_index)
 
 
-board = ChessBoard.ChessBoard()
+board = ChessBoard.ChessBoard("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
 GUI = ChessGUI(board)
 
 root = tk.Tk()
-image = tk.PhotoImage(file="chessPieces/bdd.gif")
-label = tk.Label(image=image)
-label.place(x=100, y=100)
+
 root.mainloop()
