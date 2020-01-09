05e6945a80632b0a29932c91b9d7aac9fceca3f3
Tys vanZeyl <ttis3.14@gmail.com>
2020-01-08 23:32:43 -0800

    Change rotation values

--- Code examples/Python (RPi)/Basic example/al5_startup_pos.py
+++ Code examples/Python (RPi)/Basic example/al5_startup_pos.py
@@ -9,9 +9,9 @@ import serial
 sp = serial.Serial('/dev/ttyUSB0', 9600)
 
 # set the arm to default centered position
-sp.write("#0 P1500\r".encode())
-sp.write("#1 P1500\r".encode())
-sp.write("#2 P1500\r".encode())
-sp.write("#3 P1500\r".encode())
-sp.write("#4 P1500\r".encode())
-sp.write("#5 P1500\r".encode())
+sp.write("#0 P1000\r".encode())
+sp.write("#1 P1000\r".encode())
+sp.write("#2 P1000\r".encode())
+sp.write("#3 P1000\r".encode())
+sp.write("#4 P1000\r".encode())
+sp.write("#5 P1000\r".encode())
