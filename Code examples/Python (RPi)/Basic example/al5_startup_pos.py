1f1be7ee95c7d6d70019f5cfdb2de150c86f43e6
Tys vanZeyl <ttis3.14@gmail.com>
2020-01-09 00:19:53 -0800

    Change to based on inputs

--- Code examples/Python (RPi)/Basic example/al5_startup_pos.py
+++ Code examples/Python (RPi)/Basic example/al5_startup_pos.py
@@ -9,9 +9,21 @@ import serial
 sp = serial.Serial('/dev/ttyUSB0', 9600)
 
 # set the arm to default centered position
-sp.write("#0 P2000\r".encode())
-sp.write("#1 P2000\r".encode())
-sp.write("#2 P2000\r".encode())
-sp.write("#3 P2000\r".encode())
-sp.write("#4 P2000\r".encode())
-sp.write("#5 P2000\r".encode())
+sp.write("#0 P1500\r".encode())
+sp.write("#1 P1500\r".encode())
+sp.write("#2 P1500\r".encode())
+sp.write("#3 P1500\r".encode())
+sp.write("#4 P1500\r".encode())
+sp.write("#5 P1500\r".encode())
+
+while input("continue?(y) ") == "y":
+    motor = str(input("motor number(0..5): "))
+    sp.write(("#" + motor + " P" + str(input("Pos " + motor + ": ")) + "\r").encode())
+
+# Set all motors to idle/unpowered (pulse = 0)
+print("< Idling motors... >")
+for i in range(0,6):
+    print(("#" + str(i) + " P" + str(0) + "\r").encode())
+    sp.write(("#" + str(i) + " P" + str(0) + "\r").encode())
+print("< Done >")
+
